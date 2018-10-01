1298b94a6df48eb1793ebb22a09664147a739c6f
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2018-10-01 18:25:03 +0200

    Update postprocess.py

--- postprocess.py
+++ postprocess.py
@@ -27,11 +27,12 @@ def run(entry_path, set_path, en_path, de_path, _set):
     # referring expressions
     entryset = reg.run(entryset, 'en')
 
-    stats.run(entryset)
+    lexsize, templates, templates_de, entities, references = stats.run(entryset)
 
     # run xml generator
     parser.run_generator(entryset=entryset, input_dir=set_path, output_dir=en_path, lng='en')
     parser.run_generator(entryset=entryset, input_dir=set_path, output_dir=de_path, lng='de')
+    return lexsize, templates, templates_de, entities, references
 
 if __name__ == '__main__':
     DEV_PATH = 'corpus/delexicalized/dev'
@@ -61,7 +62,7 @@ if __name__ == '__main__':
     DE_TRAIN_PATH = 'final/de/train'
     if not os.path.exists(DE_TRAIN_PATH):
         os.mkdir(DE_TRAIN_PATH)
-    run(entry_path=ENTRY_PATH, set_path=TRAIN_PATH, en_path=EN_TRAIN_PATH, de_path=DE_TRAIN_PATH, _set=_set)
+    lexsize, templates, templates_de, entities, references = run(entry_path=ENTRY_PATH, set_path=TRAIN_PATH, en_path=EN_TRAIN_PATH, de_path=DE_TRAIN_PATH, _set=_set)
 
     # DEVSET
     print 'Preparing devset...'
@@ -76,6 +77,24 @@ if __name__ == '__main__':
     DE_DEV_PATH = 'final/de/dev'
     if not os.path.exists(DE_DEV_PATH):
         os.mkdir(DE_DEV_PATH)
-    run(entry_path=ENTRY_PATH, set_path=DEV_PATH, en_path=EN_DEV_PATH, de_path=DE_DEV_PATH, _set=_set)
+    lexsize2, templates2, templates_de2, entities2, references2 = run(entry_path=ENTRY_PATH, set_path=DEV_PATH, en_path=EN_DEV_PATH, de_path=DE_DEV_PATH, _set=_set)
+    lexsize += lexsize2
+    templates.extend(templates2)
+    templates_de.extend(templates_de2)
+    entities.extend(entities2)
+    references.extend(references2)
 
-    # os.remove(ENTRY_PATH)
+    print('Number of texts: ', lexsize)
+    print('English Templates: ', len(set(templates)))
+    print('German Templates:', len(set(templates_de)))
+
+    print('Number of Entities: ', len(set(entities)))
+    print('References:', len(references))
+    names = len(filter(lambda reference: reference.reftype == 'name', references))
+    print('Names: ', names)
+    pronouns = len(filter(lambda reference: reference.reftype == 'pronoun', references))
+    print('Pronouns: ', pronouns)
+    descriptions = len(filter(lambda reference: reference.reftype == 'description', references))
+    print('Descriptions: ', descriptions)
+    demonstratives = len(filter(lambda reference: reference.reftype == 'demonstrative', references))
+    print('Demonstratives: ', demonstratives)
