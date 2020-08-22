d793a66e736e384310f67edd210bee28f751f8c9
ThiagoCF05 <thiago.castro.ferreira@gmail.com>
2020-08-22 10:01:57 -0300

    Fixing pronoun gender

--- synthetic.py
+++ synthetic.py
@@ -106,98 +106,124 @@ for size in range(2, 8):
         entitymap = entry.entitymap_to_dict()
         entity2tag = { item[1]:item[0] for item in entitymap.items() }
 
-        delexicalized = []
-        select = []
-        for i, triple in enumerate(entry.modifiedtripleset):
-            subj = entity2tag[triple.subject].replace('-', '_')
+        try:
+            delexicalized = []
+            select = []
+            for i, triple in enumerate(entry.modifiedtripleset):
+                subj = entity2tag[triple.subject].replace('-', '_')
 
-            pred = entry.originaltripleset[i].predicate
-            obj = entity2tag[triple.object].replace('-', '_')
-            delexicalized.append((subj, pred, obj))
+                pred = entry.originaltripleset[i].predicate
+                obj = entity2tag[triple.object].replace('-', '_')
+                delexicalized.append((subj, pred, obj))
 
-        delexicalized = tuple(sorted(delexicalized))
-        new_entry = {
-            'category': category,
-            'triples': delexicalized,
-            'size': "2",
-            'templates': []
-        }
+            delexicalized = tuple(sorted(delexicalized))
 
-        # select first triple based on the verbalizations of 1 triple
-        first_triple = delexicalized[0]
-        pred = first_triple[1]
-        f = [w for w in new_entryset if w['triples'][1] == pred and w['category'] == category]
-        for ftriple in f:
-            entitymap = { first_triple[0]: ftriple['triples'][0], first_triple[2]: ftriple['triples'][2] }
-            triples = [ftriple['triples']] + list(delexicalized)[1:]
+            # select first triple based on the verbalizations of 1 triple
+            first_triple = delexicalized[0]
+            pred = first_triple[1]
+            f = [w for w in new_entryset if w['triples'][1] == pred and w['category'] == category]
+            for ftriple in f:
+                entitymap = { first_triple[0]: ftriple['triples'][0], first_triple[2]: ftriple['triples'][2] }
+                triples = [ftriple['triples']] + list(delexicalized)[1:]
 
-            # entitymap complete
-            for i, triple in enumerate(triples):
-                if triple[0] in entitymap:
-                    triples[i] = (entitymap[triple[0]], triple[1], triple[2])
-                elif triple[2] in entitymap:
-                    triples[i] = (triple[0], triple[1], entitymap[triple[2]])
+                # entitymap complete
+                for i, triple in enumerate(triples):
+                    if triple[0] in entitymap:
+                        triples[i] = (entitymap[triple[0]], triple[1], triple[2])
+                    elif triple[2] in entitymap:
+                        triples[i] = (triple[0], triple[1], entitymap[triple[2]])
 
-            # SPARQL complete
-            select, where = [], []
-            for triple in triples:
-                subj, pred, obj = triple
+                # SPARQL complete
+                select, where = [], []
+                for triple in triples:
+                    subj, pred, obj = triple
 
-                sparql_subj = '<http://dbpedia.org/resource/'+subj+ '>'
-                sparql_obj = '<http://dbpedia.org/resource/'+obj+ '>'
-                if subj.split('_')[0] in ['AGENT', 'BRIDGE']:
-                    select.append('?' + subj)
-                    sparql_subj = '?' + subj
-                if obj.split('_')[0] in ['PATIENT', 'BRIDGE']:
-                    select.append('?' + obj)
-                    sparql_obj = '?' + obj
+                    sparql_subj = '<http://dbpedia.org/resource/'+subj+ '>'
+                    sparql_obj = '<http://dbpedia.org/resource/'+obj+ '>'
+                    if subj.split('_')[0] in ['AGENT', 'BRIDGE']:
+                        select.append('?' + subj)
+                        sparql_subj = '?' + subj
+                    if obj.split('_')[0] in ['PATIENT', 'BRIDGE']:
+                        select.append('?' + obj)
+                        sparql_obj = '?' + obj
 
-                where.append(' '.join([sparql_subj, 'dbo:' + pred, sparql_obj, '.']))
-            where = '\n'.join(where)
+                    where.append(' '.join([sparql_subj, 'dbo:' + pred, sparql_obj, '.']))
+                where = '\n'.join(where)
 
-            select = ', '.join(list(set(select)))
-            query = """SELECT _SELECT_ WHERE { _WHERE_ }""".replace('_SELECT_', select).replace('_WHERE_', where)
-            try:
-                sparql.setQuery(query)
-                sparql.setReturnFormat(JSON)
-                results = sparql.query().convert()
-                for result in results["results"]["bindings"]:
-                    for tag in result:
-                        entity = result[tag]['value'].split('/')[-1]
-                        entitymap[tag] = entity
-                        for i, triple in enumerate(triples):
-                            if tag == triple[0]:
-                                triples[i] = (entity, triple[1], triple[2])
-                            elif tag == triple[2]:
-                                triples[i] = (triple[0], triple[1], entity)
+                select = ', '.join(list(set(select)))
+                query = """SELECT _SELECT_ WHERE { _WHERE_ }""".replace('_SELECT_', select).replace('_WHERE_', where)
+                try:
+                    sparql.setQuery(query)
+                    sparql.setReturnFormat(JSON)
+                    results = sparql.query().convert()
+                    for result in results["results"]["bindings"]:
+                        for tag in result:
+                            entity = result[tag]['value'].split('/')[-1]
+                            entitymap[tag] = entity
+                            for i, triple in enumerate(triples):
+                                if tag == triple[0]:
+                                    triples[i] = (entity, triple[1], triple[2])
+                                elif tag == triple[2]:
+                                    triples[i] = (triple[0], triple[1], entity)
 
-                # For the triples completed, verbalize the templates
-                if len(results["results"]["bindings"]) > 0:
-                    for lexEntry in entry.lexEntries:
-                        template = lexEntry.template
+                    new_entry = {
+                        'category': category,
+                        'triples': tuple(triples),
+                        'size': str(size),
+                        'templates': []
+                    }
 
-                        for reference in sorted(lexEntry.references, key= lambda x: x.number):
-                            tag = reference.tag
-                            entity = entitymap[tag.replace('-', '_')]
-                            reftype = reference.reftype
-                            regex = reference.refex
+                    # For the triples completed, verbalize the templates
+                    if len(results["results"]["bindings"]) > 0:
+                        for lexEntry in entry.lexEntries:
+                            template = lexEntry.template
 
-                            if reftype in ['name', 'description']:
-                                ref = entity.replace('_', ' ').strip()
-                                template = template.replace(tag, ref, 1)
-                            elif reftype == 'pronoun':
-                                if regex.lower().strip() in ['he', 'she']:
-                                    template = template.replace(tag, 'he', 1)
-                                elif regex.lower().strip() in ['it']:
-                                    template = template.replace(tag, 'it', 1)
-                                elif regex.lower().strip() in ['his', 'her']:
-                                    template = template.replace(tag, 'his', 1)
-                                elif regex.lower().strip() in ['its']:
-                                    template = template.replace(tag, 'its', 1)
-                        new_entry['templates'].append(template)
-            except:
-                print('ERROR')
-        if len(new_entry['templates']) > 0:
-            data.append(new_entry)
+                            for reference in sorted(lexEntry.references, key= lambda x: x.number):
+                                tag = reference.tag
+                                entity = entitymap[tag.replace('-', '_')]
+                                reftype = reference.reftype
+                                regex = reference.refex
+
+                                if reftype in ['name', 'description']:
+                                    ref = entity.replace('_', ' ').strip()
+                                    template = template.replace(tag, ref, 1)
+                                elif reftype == 'pronoun':
+                                    if regex.lower().strip() in ['he', 'she']:
+                                        pronoun = 'he'
+                                        sparql.setQuery("""
+                                            SELECT ?gender
+                                            WHERE { dbr:ENTITY foaf:gender ?gender . }
+                                        """.replace('ENTITY', entity))
+                                        sparql.setReturnFormat(JSON)
+                                        results = sparql.query().convert()
+                                        for result in results["results"]["bindings"]:
+                                            if 'gender' in result:
+                                                if result['gender']['value'] == 'female':
+                                                    pronoun = 'she'
+                                        template = template.replace(tag, pronoun, 1)
+                                    elif regex.lower().strip() in ['it']:
+                                        template = template.replace(tag, 'it', 1)
+                                    elif regex.lower().strip() in ['his', 'her']:
+                                        pronoun = 'his'
+                                        sparql.setQuery("""
+                                            SELECT ?gender
+                                            WHERE { dbr:ENTITY foaf:gender ?gender . }
+                                        """.replace('ENTITY', entity))
+                                        sparql.setReturnFormat(JSON)
+                                        results = sparql.query().convert()
+                                        for result in results["results"]["bindings"]:
+                                            if 'gender' in result:
+                                                if result['gender']['value'] == 'female':
+                                                    pronoun = 'her'
+                                        template = template.replace(tag, pronoun, 1)
+                                    elif regex.lower().strip() in ['its']:
+                                        template = template.replace(tag, 'its', 1)
+                            new_entry['templates'].append(template)
+                except:
+                    print('ERROR')
+            if len(new_entry['templates']) > 0:
+                data.append(new_entry)
+        except:
+            print('INITIAL ERROR')
 
     json.dump(data, open('data/' + str(size) + 'triples.json', 'w'), separators=(',', ':'), sort_keys=True, indent=4)
